c90d268a21d83332cb6a18ec0571fed03f88109d
Aditya Pratap Sngh Rajput <apsrcreatix@gmail.com>
2018-10-23 01:00:11 +0530

    first release

--- userinfo/user-json.js
+++ userinfo/user-json.js
@@ -1,7 +1,7 @@
 // Edit this JSON object to make change 
 let user = {
-    "title" : "Title",
-    "name" : "Full Name",
-    "biography": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi corrupti adipisci beatae doloremque tempora fugiat quidem, magni animi cum tenetur perspiciatis omnis eveniet ab consectetur, ad, possimus cumque qui voluptatem! Resume Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim voluptatibus ad voluptate, voluptatem nulla nesciunt. Fugit aut, iusto illo vero, fuga cumque consequatur laboriosam reiciendis dolores maiores nisi a voluptates?",
-    "description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi corrupti adipisci beatae doloremque tempora fugiat quidem, magni animi cum tenetur perspiciatis omnis eveniet ab consectetur, ad, possimus cumque qui voluptatem! Resume Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim voluptatibus ad voluptate, voluptatem nulla nesciunt. Fugit aut, iusto illo vero, fuga cumque consequatur laboriosam reiciendis dolores maiores nisi a voluptates? "
+    "title" : "UNVEIL | Portfolio Website",
+    "name" : "unveil",
+    "biography": "Are you looking for a quick contact website portfolio with minimal efforts ? <br> Unveil is a template works with github pages and gives you contact website in just few minutes. ! <br> Go to github repository link given below for more.",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
 };
