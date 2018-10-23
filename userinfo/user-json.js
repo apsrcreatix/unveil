a8137af90aca9df81d3275bb8dc74e9599b19b85
Aditya Pratap Sngh Rajput <apsrcreatix@gmail.com>
2018-10-23 14:44:22 +0530

    add property to user-json

--- userinfo/user-json.js
+++ userinfo/user-json.js
@@ -3,5 +3,18 @@ let user = {
     "title" : "UNVEIL | Portfolio Website",
     "name" : "unveil",
     "biography": "Are you looking for a quick contact website portfolio with minimal efforts ? <br> Unveil is a template works with github pages and gives you contact website in just few minutes. ! <br> Go to github repository link given below for more.",
-    "description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    "links" : {
+        "github" : "https://www.github.com/apsrcreatix/unveil",
+        "twitter" : "#",
+        "facebook" : "#",
+        "dribble" : "#",
+        "instagram" : "#",
+        "whatsapp" : "https://wa.me/YOUR-NUMBER-HERE/?text=Hey+YOUR-NAME+%21+I+am+",
+        "linkedin" : "#",
+        "pintrest" : "#",
+        "youtube" : "#",
+    },
+    "primary-color" : "#414345",
+    "secondary-color" : "#232526"
 };
