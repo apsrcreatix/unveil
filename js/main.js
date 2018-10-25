7bd4ffb9e9e38c826ff60d60f48c20369ed907a2
Aditya Pratap Sngh Rajput <apsrcreatix@gmail.com>
2018-10-25 16:25:10 +0530

    UI fixes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,52 @@
 let title = document.getElementById("title");
 let fullname = document.getElementById("fullname");
 let biography = document.getElementById("biography");
+let profile_photo = document.getElementById("user-image");
+let allGradientText = document.getElementsByClassName("gradient-text");
+let corner_button = document.getElementById("corner-button");
+let heading;
+const primary_color = user.colors["primary-color"];
+const secondary_color = user.colors["secondary-color"];
+document.getElementsByTagName("style")[0].innerHTML = `.gradient-text{background:${primary_color}; background:-webkit-linear-gradient(to right, ${primary_color},${secondary_color}); background:linear-gradient(to right,${primary_color},${secondary_color});
+ -webkit-background-clip:text; background-clip:text; -webkit-text-fill-color: transparent;}`;
 // changing inner html i.e, DOM
 title.innerHTML = user.title;
 fullname.innerHTML = user.name;
 biography.innerHTML = user.biography;
+profile_photo.setAttribute('src',user.photo);
+
+profile_photo.style = `-webkit-filter: drop-shadow(0 0 0.75rem ${primary_color});
+     filter: drop-shadow(0 0 0.75rem ${primary_color});`;
+
+fullname.style=allGradientText.style=corner_button.style=`background:${primary_color}; background:-webkit-linear-gradient(to right, ${primary_color},${secondary_color}); background:linear-gradient(to right,${primary_color},${secondary_color});
+ -webkit-background-clip:text; background-clip:text; -webkit-text-fill-color: transparent;`;
+
+
+// setting links
+ function goToGithub(){
+    window.location = user.links.github;
+ }
+ function goToTwitter(){
+    window.location = user.links.twitter;
+ }
+ function goToFacebook(){
+    window.location = user.links.facebook;
+ }
+ function goToDribble(){
+    window.location = user.links.dribble;
+ }
+ function goToInstagram(){
+    window.location = user.links.instagram;
+ }
+ function goToWhatsapp(){
+    window.location = user.links.whatsapp;
+ }
+ function goToLinkedin(){
+    window.location = user.links.linkedin;
+ }
+ function goToPintrest(){
+    window.location = user.links.pintrest;
+ }
+ function goToYoutube(){
+    window.location = user.links.youtube;
+ }
--- userinfo/user-json.js
+++ userinfo/user-json.js
@@ -1,20 +1,23 @@
 // Edit this JSON object to make change 
-let user = {
+const user = {
     "title" : "UNVEIL | Portfolio Website",
+    "photo": "userinfo/profile-photo/photo.png",
     "name" : "unveil",
     "biography": "Are you looking for a quick contact website portfolio with minimal efforts ? <br> Unveil is a template works with github pages and gives you contact website in just few minutes. ! <br> Go to github repository link given below for more.",
     "description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
     "links" : {
         "github" : "https://www.github.com/apsrcreatix/unveil",
-        "twitter" : "#",
+        "twitter": "https://twitter.com/USERNAME",
         "facebook" : "#",
         "dribble" : "#",
-        "instagram" : "#",
+        "instagram" : "https://www.instagram.com/USERNAME",
         "whatsapp" : "https://wa.me/YOUR-NUMBER-HERE/?text=Hey+YOUR-NAME+%21+I+am+",
         "linkedin" : "#",
-        "pintrest" : "#",
+        "pinterest" : "#",
         "youtube" : "#",
     },
-    "primary-color" : "#414345",
-    "secondary-color" : "#232526"
+    "colors" : {
+    "primary-color": "#414345",
+    "secondary-color": "#232526"
+    }
 };
